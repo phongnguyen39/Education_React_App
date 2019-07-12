2bdc46329df45364f5fad1e87537b22cde68c688
phong_nguyen <phong.nguyen39@gmail.com>
2019-07-12 13:50:17 -0700

    restyled page, added ability to get description of word and a link to wikipedia

--- src/App.js
+++ src/App.js
@@ -9,36 +9,55 @@ class App extends Component {
     letters: 'Aa',
     words: [],
     otherState: '',
-    word:''
+    word: [],
+    description: '',
+    link: ''
   }
 
   randomize = () => {
-    let url= 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+    let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
     const alpha = "abcdefghijklmnopqrstuvwxyz";
     let rand = Math.floor(Math.random() * 26)
     let output = alpha.charAt(rand).toUpperCase() + " " + alpha.charAt(rand).toLowerCase();
     this.setState({
-      letters:output
+      letters: output
     })
     let search = url + output[2]
     console.log(output);
     this.fetchFromAPI(search)
+    return output;
   }
 
   selectLetter = (props) => {
+    let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
+    let rand = Math.floor(Math.random() * 26)
+    let output = alpha.charAt(rand).toUpperCase() + " " + alpha.charAt(rand).toLowerCase();
     this.setState({
       letters: props + props.toLowerCase()
     })
+    let search = url + props + output + output;
+    console.log(search)
+    this.fetchFromAPI(search)
   }
-  
+
   fetchFromAPI = (url) => {
     fetch(url)
-    .then((response) => response.json())
-    .then(data => {
-        this.setState({words:data, word:data[1][1]})
-        console.log(this.state.words);
-    })
-   
+      .then((response) => response.json())
+      .then(data => {
+        if (data[1][1] == null) {
+          console.log('error')
+          return;
+        } else {
+          this.setState({
+            words: data,
+            word: data[1][1],
+            description: data[2][1],
+            link: data[3][1]
+          })
+          console.log(this.state.words);
+        }
+      })
   }
 
   render() {
@@ -46,7 +65,7 @@ class App extends Component {
       font: '1.5em'
     }
     let categoryLetters = null;
-    const capLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+    const capLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
     categoryLetters = (
       <div id="alphabet">
         {capLetter.map((letter, index) => {
@@ -60,16 +79,23 @@ class App extends Component {
         <h1 id="title">Fun with Letters and Words</h1>
         <p>Select a letter</p>
         {categoryLetters}
-        <h1 id="lg-letter">
-          {/* <Letter style = {style} letters={this.state.letters[0]}>{this.state.letters[0]}</Letter> */}
-          <Letter style = {style} letter={this.state.letters}/>
-        </h1>
-        <button onClick={this.randomize}>Random Letter</button>
-        
-        <Word 
-          id='word' 
-          randomWord={this.state.word} 
-        />
+        <div id="container">
+      
+            <h1 id="lg-letter">
+              <Letter style={style} letter={this.state.letters} />
+            </h1>
+            
+
+          <button onClick={this.randomize}>Random Letter</button>
+
+          <div id='word'>
+            <Word
+              randomWord={this.state.word}
+              wordDescription={this.state.description}
+              wordLink={this.state.link}
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/WordGenerator/Word.js
+++ src/WordGenerator/Word.js
@@ -4,7 +4,12 @@ import './Word.css';
 const word = (props) => {
     return(
         <div>
-            <p>{props.randomWord}</p>
+            <h2>Wiki Word of the Day</h2>
+            <p className="word">{props.randomWord}</p>
+            <p className='description'>
+            {props.wordDescription}
+            </p>
+            <a href='{props.wordLink}' className='link'>{props.wordLink}</a>
         </div>
     );
 };
