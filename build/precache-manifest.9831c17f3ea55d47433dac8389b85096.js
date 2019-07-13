79b105df13cc39fe918d4f171d34c7f5631b4797
phong_nguyen <phong.nguyen39@gmail.com>
2019-07-13 11:21:12 -0700

    removed /build from .gitignore file so it can be built using Netlify

---
+++ build/precache-manifest.9831c17f3ea55d47433dac8389b85096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47055e73775a3ef69e1e08b23ad63835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049d42c849fcc1cdcf3a",
+    "url": "/static/css/main.36d7095f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "049d42c849fcc1cdcf3a",
+    "url": "/static/js/main.d2b1ec54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
